7b1debaf1e34a1650f5fe55e001a00d5b5d76f36
3Natasha <ncseaman2@frontier.com>
2024-12-22 19:25:20 -0500

    It works!!! All of the drive,lift, arm and claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telop.java
@@ -212,7 +212,7 @@ public class Telop extends LinearOpMode {
 
 
             // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+           // telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
